69a6ab3e5503e41170ca121ad22c819eab709847
Dorian <dorianemenir@gmail.com>
2022-03-05 12:11:58 +0100

    :construction: typo fix

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 const App = () => {
   return (
     <div>
-      <h3 className='text-3xl font-bold underline bg-[#1da1f2]'>Google APp</h3>
+      <h3 className='text-3xl font-bold underline bg-[#1da1f2]'>Google App</h3>
     </div>
   );
 };
