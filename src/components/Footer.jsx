4f9d023cfb15e98ee2a24f7ddaee3c0bea2dd85a
Pannu786 <thepannu786@gmail.com>
2022-03-06 23:30:14 +0530

    added routing

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 export const Footer = () => (
-  <div>
-    <h1>Footer</h1>
+  <div className='text-purple-500 text-center p-10 mt-10 border-t bg-cream-700 dark:bg-gray-800 dark:text-blue-200 '>
+    <h1>2022 G👀gle, pvt.ltm. </h1>
   </div>
 );
---
+++ src/components/Routing.jsx
@@ -0,0 +1,24 @@
+import { Route, Routes, Navigate } from 'react-router-dom';
+import { Results } from './Results';
+
+export const Routing = () => (
+  <div className='p-4'>
+    {/* using version-6 routing */}
+
+    <Routes>
+      <Route path='/' element={<Navigate to='/search' />} />
+
+      {/* this is short way */}
+      {['news', 'images', 'videos', 'search'].map((path) => (
+        <Route key={path} path={path} element={<Results />} />
+      ))}
+
+      {/* this is long way of doing */}
+
+      {/* <Route path='/news' element={<Results />} />
+        <Route path='/images' element={<Results />} />
+        <Route path='/videos' element={<Results />} />
+         <Routes path='/search' element={<Results />} /> */}
+    </Routes>
+  </div>
+);
